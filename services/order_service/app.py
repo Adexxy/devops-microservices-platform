b178290b2eab9118c8e976c9f39dc8fe827ed402
Owoade Taiwo <adexxy@live.com>
2025-05-22 10:05:58 +0100

    added containerisation

--- services/order_service/app.py
+++ services/order_service/app.py
@@ -34,4 +34,4 @@ def create_order():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5003)
--- services/product_service/app.py
+++ services/product_service/app.py
@@ -37,4 +37,4 @@ def create_product():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5002)
